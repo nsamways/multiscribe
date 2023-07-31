848d76cd8f26a1097a710804ab72cfe20082b3aa
nsamways <nsamways@localhost>
2023-07-31 16:56:07 +0100

    Added parser arguments

--- multiScribe/src/main.py
+++ multiScribe/src/main.py
@@ -9,7 +9,7 @@ import os
 import whisper
 import json
 import ffmpeg
-
+import argparse
     
 # Function to recursively search for MP4 files in the current directory
 def search_files(directory):
@@ -26,8 +26,9 @@ def extract_audio(mp4_file):
     # check if the file already exists. If not, create it
     audio_file = mp4_file[:-4] + ".wav"
     
-    if os.path.isfile(audio_file):
-    
+    if not os.path.isfile(audio_file):
+        
+        # doesn't exist, so create
         stream = ffmpeg.input(mp4_file)
         stream = ffmpeg.output(stream, audio_file)
         ffmpeg.run(stream)
@@ -37,7 +38,7 @@ def extract_audio(mp4_file):
 
 # Function to generate transcript using OpenAI's Whisper
 def generate_transcript(audio_file, lang_model):
-    model = whisper.load_model("small.en")
+    model = whisper.load_model(lang_model)
     transcript = model.transcribe(audio_file, fp16=False, language='English')
     return transcript
 
@@ -53,12 +54,23 @@ def process_files(directory):
 
 def main():
 
-# Provide the directory path where you want to search for MP4 files
-
 # use the info passed throught the command line, or use cwd as defauly
-    for arg in (len(sys.argv):
-        
-        if cl_arg = "-m":
+    
+
+    parser = argparse.ArgumentParser()
+
+    #-db DATABSE -u USERNAME -p PASSWORD -size 20
+    parser.add_argument("-i", "--input", help="Folder root for video files.")
+    parser.add_argument("-o", "--output", help="Output folder for transcripts. Default is same folder as video.")
+    parser.add_argument("-m", "--model", help="Language model for Whisper. Default is 'tiny-en'.")
+
+    args = parser.parse_args()
+
+    print( "input {} output {} model  ".format(
+        args.input,
+        args.output,
+        args.model
+        ))
             
     
     directory_path = "./"
