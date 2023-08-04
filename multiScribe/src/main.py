354c3b9a4f600806df2861f270222b189a7066b9
nsamways <nsamways@localhost>
2023-08-04 15:00:49 +0100

    Complete but not fully tested. Need to test transcription.

--- multiScribe/src/main.py
+++ multiScribe/src/main.py
@@ -6,7 +6,6 @@ Created on 26 Jul 2023
 
 import os
 import whisper
-import json
 import ffmpeg
 import argparse
 import time
@@ -14,7 +13,6 @@ import time
 # Function to recursively search for MP4 files in the current directory
 def search_files(directory):
     mp4_files = []
-    
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".mp4"):
@@ -25,6 +23,7 @@ def search_files(directory):
 # Function to extract audio from MP4 using python-ffmpeg
 def extract_audio(mp4_file):
     
+    c_durr = None
     # check if the file already exists. If not, create it
     audio_file = mp4_file[:-4] + ".wav"
     
@@ -32,26 +31,37 @@ def extract_audio(mp4_file):
         
         # doesn't exist, so create
         print("Creating {} ... ".format(audio_file))
+        
+        c_start = time.perf_counter()
+        
         stream = ffmpeg.input(mp4_file)
         stream = ffmpeg.output(stream, audio_file, loglevel="quiet")
         ffmpeg.run(stream)
+        
+        c_durr = time.perf_counter() - c_start
 
-    return audio_file
+    return (c_durr, audio_file)
 
 
 # Function to generate transcript using OpenAI's Whisper
 def generate_transcript(audio_file, lang_model, processor):
     
     # check if model already exists
-    start = time.perf_counter()
-    
-    print("Loading model")
-    model = whisper.load_model(lang_model, device=processor)
+    try:
+        model
+    except NameError:
+        # doesn't exist, so create it
+        print("Loading model")
+        model = whisper.load_model(lang_model, device=processor)
+ 
     print("Generating transcript")
+
+    t_start = time.perf_counter()
     transcript = model.transcribe(audio_file, fp16=False, language='English')
+    t_durr = time.perf_counter() - t_start
     
-    return transcript
-    print(f"Total duration {time.perf_counter()-start}")
+    return (t_durr, transcript)
+    
     
 
 def main():
@@ -70,7 +80,7 @@ def main():
     args = parser.parse_args()
 
     input_path = args.input if args.input else "./"
-    output_path = args.output if args.output else  None
+    output_path = args.output if args.output else  "./"
     whisper_model = args.model if args.model else "base"
     proc_type = "cuda" if args.processor == "g" else "cpu" 
 
@@ -78,16 +88,77 @@ def main():
     mp4_files = search_files(input_path)
 
     # print(mp4_files)
-    
-    # process file list accordingly
-    for mp4_file in mp4_files:
-        audio_file = extract_audio(mp4_file)
-        transcript = generate_transcript(audio_file, whisper_model, proc_type)
-        #
-        print(f"Transcript for {mp4_file}:")
-        print(transcript)
+    if mp4_files:
+        # there are some MP4 files so set everything up
+        if output_path:
+            # create a new folder to put the transcripts in
+            custom_output = True
+   
+            new_path = output_path
+             
+            if not os.path.exists(new_path):
+                os.makedirs(new_path)
+            
+            # create the ouptput file      
+            f =  open((os.path.join(new_path, r'./log.txt')), 'w')
+            f.write("\n Results")
+      
+            
+        else:
+            # just create the logfile
+
+            f = open(r'./log.txt', 'w')
+            f.write("\n Results2")
+      
+        # write the configuration to the log file
+            
+        f.write("\n Input root: " +  input_path)
+        f.write("\n Output base: " +  output_path)
+        f.write("\n Whisper model: " +  whisper_model)
+        f.write("\n Processor: " +  proc_type)
         
-        words = transcript["text"]
+        f.flush()    
+        # generate base for transcript output    
+        
+        # process file list accordingly
+        for mp4_file in mp4_files:
+            
+            (cv_duration, audio_file) = extract_audio(mp4_file)
+            (tr_duration, transcript) = generate_transcript(audio_file, whisper_model, proc_type)
+            
+            # write details to log file
+            if cv_duration:
+                f.writelines("\n " + mp4_file + " Converstion time: " + cv_duration + " Transcription time: " + tr_duration)
+    
+            else:
+                f.writelines("\n " + mp4_file + " Transcription time: " + tr_duration)
+                
+            # output transcript
+            words = transcript["text"]
+            
+            if custom_output:
+                
+                head_tail = os.path.split(mp4_file)
+                new_head = head_tail[1][:-4] + ".txt"
+                tr_out_path = os.path.join(new_path, new_head)
+                    
+            else:
+                tr_out_path = mp4_file[:-4] + ".txt"
+            
+            
+            with open(tr_out_path ,'w') as g: 
+                g.writelines(words)
+            
+            
+        # close the log file
+        f.close()
+                
+    else:
+        
+        print("No MP4 files found. Exiting.")        
+        
+
+    
     
 if __name__ == '__main__':
     main()
